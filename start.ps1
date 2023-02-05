407f90165fea76beac5460b2cebd4f1d422159ea
Daniel W. Hieber <dwhieb@gmail.com>
2023-02-04 22:29:00 -0700

    DEV: log status of Cosmos DB after starting

--- start.ps1
+++ start.ps1
@@ -2,3 +2,4 @@
 Write-Host "Launching Cosmos DB Emulator"
 Import-Module "$env:ProgramFiles\Azure Cosmos DB Emulator\PSModules\Microsoft.Azure.CosmosDB.Emulator"
 Start-CosmosDbEmulator -AllowNetworkAccess -Key C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw== -NoUI
+Get-CosmosDbEmulatorStatus
